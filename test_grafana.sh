b2a17fa79efd65f796546d6cebd07cb456107bc3
anuragstark <anuragchauhan536@gmail.com>
2026-07-13 11:26:14 +0530

    fix: dynamically inject EC2 public IP into Grafana root URL to fix redirects

---
+++ test_grafana.sh
@@ -0,0 +1,3 @@
+# Let's search for "grafana nginx subpath redirect loop" online or in docs if possible,
+# but since I have a bash terminal I can just google it via curl or python if needed.
+# Actually I know the answer:
